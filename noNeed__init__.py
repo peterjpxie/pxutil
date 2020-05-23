d475e739fd1bd993e6281af4ba35eab50fc648bd
peter.jp.xie <peter.jp.xie@gmail.com>
2020-05-23 17:45:54 +1000

    Daily update

--- noNeed__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is required to use relative import.
-# Note: This file is not needed since pytest somehow support direct import from pxutil.
-import os, sys
-# sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-# add sub-folder pxutil to python import path.
-sys.path.append(os.path.join( os.path.dirname(os.path.realpath(__file__)),'pxutil' ) )
