12aff01f527baa883d45bfe973204da937cc0982
peter.jp.xie <peter.jp.xie@gmail.com>
2020-08-08 18:00:18 +1000

    Daily update

--- pxutil/__init__.py
+++ pxutil/__init__.py
@@ -1,16 +1,15 @@
 """
-    pxutil
-    ~~~~~
+pxutil
+~~~~~
 
-    Peter Jiping Xie's personal utils.
+Peter Jiping Xie's personal utils.
 
-    :copyright: © 2019 by Peter Jiping Xie.
-    :license: MIT.
+:copyright: © 2019 by Peter Jiping Xie.
+:license: MIT.
 """
 
-# __version__ = '0.1'
+# Control the version directly in setup.py
+# __version__ = '0.0.1'
 
 # import func/classes from .pxutil here so users can them directly, i.e. pxutil.bash() instead of pxutl.pxutil.bash().
 from .pxutil import bash, trim_docstring, grep
-
-# from .pxutil import *
