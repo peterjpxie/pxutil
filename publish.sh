63d68532e44d6efcfdccb96c61a9b2cd08c1b5b8
peter.jp.xie <peter.jp.xie@gmail.com>
2022-02-03 10:50:21 +0800

    update

--- publish.sh
+++ publish.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # 
 # Publish package to PyPi
+# Note: This script is obselete as we include cython and need to pulish cross-platform builds 
+# through cibuildwheel using Github Actions CI.
 set -x
 
 rm -rf dist/* && \
