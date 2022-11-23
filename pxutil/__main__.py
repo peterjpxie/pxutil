dcacd89e62a4aebb6d20be18bf4c409b0aea1a4b
peter.jp.xie <peter.jp.xie@gmail.com>
2022-11-24 09:03:17 +1100

    add __main__.py for python -m pxutil

--- pxutil/__main__.py
+++ pxutil/__main__.py
@@ -7,15 +7,6 @@ https://docs.python.org/3/library/__main__.html
 pip.__main__.py
 """
 import argparse
-import os
-import sys
-
-# Remove '' and current working directory from the first entry
-# of sys.path, if present to avoid using current directory
-# in pip commands check, freeze, install, list and show,
-# when invoked as python -m pip <command>
-if sys.path[0] in ("", os.getcwd()):
-    sys.path.pop(0)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='This is a sample __main__.py to execute as `python -m pxutil.`')
