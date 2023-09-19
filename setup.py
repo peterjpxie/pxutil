d97e85fe992737593b757edc1c09e2773f26755c
Peter Jiping Xie <peter.jp.xie@gmail.com>
2023-09-20 07:17:40 +1000

    add py311

--- setup.py
+++ setup.py
@@ -27,30 +27,31 @@ setup(
     author_email="peter.jp.xie@gmail.com",
     url="https://github.com/peterjpxie/pxutil.git",
     license="MIT",
-    packages=find_packages(), 
+    packages=find_packages(),
     python_requires='>=3.7', # temp: support 3.7 for now though officially built for 3.8+
     install_requires=[
         'requests>=2.0.0',
-    ],    
+    ],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: MacOS", 
+        "Operating System :: MacOS",
         "Operating System :: POSIX",
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",             
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Software Development :: Libraries",
     ],
-    # build cython modules  
-    ext_modules=cythonize("*/*_cy.py"), # pure python with cython type annotation  
+    # build cython modules
+    ext_modules=cythonize("*/*_cy.py"), # pure python with cython type annotation
     # ext_modules=cythonize("*/*.pyx"), # language_level="3"
     # ext_modules=cythonize(extensions),
-    zip_safe=False,   
+    zip_safe=False,
     # shared lib from Cython - No need! setup.py build and include them in the wheel automatically.
-    # package_data={'': ['pxutil/*.so', 'pxutil/*.dll', 'pxutil/*.pyd']}, 
+    # package_data={'': ['pxutil/*.so', 'pxutil/*.dll', 'pxutil/*.pyd']},
     # cli
     entry_points = {
         'console_scripts': ['px.loop=pxutil.cli:loop_main'],
