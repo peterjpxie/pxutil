a4f4b8e582d0435355580a4eff5e2e94d7e3a09d
Peter Jiping Xie <peter.jp.xie@gmail.com>
2026-09-16 16:45:56 +1000

    0.1.0 release

--- pxutil/cli.py
+++ pxutil/cli.py
@@ -20,7 +20,7 @@ from pxutil import bashx, register_signal_ctrl_c, ChatAPI
 import pxutil as px
 
 # defaults
-Chat_Model_Default = "deepseek-v4-flash"
+Chat_Model_Default = "deepseek-flash"
 
 
 def loop_main():
